0754edbf500f613f7e2da5a64944c04ec99f181f
JesperMjornman <47789590+JesperMjornman@users.noreply.github.com>
2019-02-22 10:28:21 +0100

    Update MUX_TEST.asm

--- MUX_TEST.asm
+++ MUX_TEST.asm
@@ -40,16 +40,12 @@ VMEM_INIT:
 	ldi		ZH, HIGH(VMEM)
 	ldi		ZL, LOW(VMEM)
 	clr		r16
-	ldi		r17, $FF
+	ldi		r18, $FF
+	ldi		r17, $10
 VMEM_SET:
 	st		Z+,	r17
 	inc		r16
-	sbrs	r16, 3
-	rjmp	VMEM_SET
-	inc		r16
-	dec		r17
-	st		Z+, r17
-	sbrs	r16, 7
+	sbrs	r16, 4
 	rjmp	VMEM_SET
 	ret
   ;----------------------------
@@ -240,6 +236,7 @@ CHECK_COLLISION:
 	st		Z, r17*/
 
 HIT:
+	call	CHECK_ROW_FILLED
 	call	BUILD_BLOCK
 END_CHECK:
 	pop		r19
@@ -269,6 +266,55 @@ BUILD_BLOCK:
 	ldi		ZL, LOW(VMEM)
 	st		Z, r16
 
+	pop		r17
+	pop		r16
+	pop		ZL
+	pop		ZH
+	ret
+
+CHECK_ROW_FILLED:
+	push	ZH
+	push	ZL
+	push	r16
+	push	r17
+	push	r18
+	push	r20
+
+	ldi		r20, 1
+	ldi		ZH, HIGH(VMEM)
+UPDATE_LOOP:
+	ldi		ZL, LOW(VMEM)
+	ld		r18, Z		;(Sparar värdet)
+	add		ZL, r20
+	ld		r17, Z
+	cpi		r17, $00
+	brne	DONE_ROW
+	mov		r16, r20	; Intern loop counter för FULL_ROW_FOUND
+
+FULL_ROW_FOUND:
+	dec		r16			; Kolla raden ovanför den fulla
+	ldi		ZL, LOW(VMEM)
+	add		ZL, r16
+	ld		r17, Z		; Z så ladda och öka ZL
+	inc		ZL
+	inc		r16			; Återställ countern
+	st		Z, r17
+
+	dec		ZL			; Tillbaks till raden ovanför
+	ldi		r17, $FF	; Nollställ raden
+	st		Z, r17		; Spara nollställningen
+	
+	dec		r16
+	cpi		r16, 0
+	brne	FULL_ROW_FOUND
+
+DONE_ROW:
+	inc		r20
+	cpi		r20, $10
+	brne	UPDATE_LOOP
+
+	pop		r20
+	pop		r18
 	pop		r17
 	pop		r16
 	pop		ZL
