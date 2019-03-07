4a272c3be3d4446541da6b8bbe42706a103574b0
DanMas94 <47330483+DanMas94@users.noreply.github.com>
2019-03-07 08:56:32 +0100

    Update SOUND.asm

--- SOUND.asm
+++ SOUND.asm
@@ -12,34 +12,92 @@
 	out		DDRC, r16
 
 	START:
-	sbis	PINA, 0
-	rjmp	START
-	call    PLAY
+	sbic	PINA, 0
+	rjmp	PLAY1
+
+	sbic	PINA, 1
+	rjmp	PLAY1
+
+	sbic	PINA,2
+	rjmp	PLAY2
+
+	call    START
 	call	WAIT
 
-PLAY:
-SOUND:
+PLAY1:
  ldi r16, $0A
-SOUND_LOOP:
+SOUND_LOOP1:
  sbi PORTC, 1
- call delay
+ call delay1
  cbi PORTC, 1
- call delay
+ call delay1
  dec r16
  cpi r16, 0
- brne SOUND_LOOP
+ brne SOUND_LOOP1
  ret
-DELAY:
+DELAY1:
  push r18
  push r17
  ldi r18, 10
-delayYttreLoop:
+delayYttreLoop1:
  ldi r17,$FF
-delayInreLoop:
+delayInreLoop1:
  dec r17
- brne delayInreLoop
+ brne delayInreLoop1
  dec r18
- brne delayYttreLoop
+ brne delayYttreLoop1
+ pop r17
+ pop r18
+ ret
+
+ PLAY2:
+ ldi r16, $C7
+SOUND_LOOP2:
+ sbi PORTC, 1
+ call delay2
+ cbi PORTC, 1
+ call delay2
+ dec r16
+ cpi r16, 0
+ brne SOUND_LOOP2
+ ret
+DELAY2:
+ push r18
+ push r17
+ ldi r18, 10
+delayYttreLoop2:
+ ldi r17,$FF
+delayInreLoop2:
+ dec r17
+ brne delayInreLoop2
+ dec r18
+ brne delayYttreLoop2
+ pop r17
+ pop r18
+ ret
+
+ PLAY3:
+ ldi r16, $FF
+SOUND_LOOP3:
+ sbi PORTC, 1
+ call delay3
+ cbi PORTC, 1
+ call delay3
+ dec r16
+ cpi r16, 0
+ brne SOUND_LOOP3
+ ret
+DELAY3:
+ push r18
+ push r17
+ ldi r18, 10
+delayYttreLoop3:
+ ldi r17,$FF
+delayInreLoop3:
+ dec r17
+ brne delayInreLoop3
+ dec r18
+ brne delayYttreLoop3
  pop r17
  pop r18
  ret
