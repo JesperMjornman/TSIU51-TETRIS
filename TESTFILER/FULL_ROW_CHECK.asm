cb60929d7461a8617507782d92660bb8927eca44
JesperMjornman <47789590+JesperMjornman@users.noreply.github.com>
2019-12-06 15:58:04 +0100

    Update FULL_ROW_CHECK.asm

--- TESTFILER/FULL_ROW_CHECK.asm
+++ TESTFILER/FULL_ROW_CHECK.asm
@@ -1,4 +1,4 @@
-CHECK_ROW_FILLED:
+	CHECK_ROW_FILLED:
 	push	ZH
 	push	ZL
 	push	r16
@@ -8,37 +8,37 @@ CHECK_ROW_FILLED:
 
 	ldi		r20, 1
 	ldi		ZH, HIGH(VMEM)
-UPDATE_LOOP:
+	UPDATE_LOOP:
 	ldi		ZL, LOW(VMEM)
-	ld		r18, Z		;(Sparar värdet)
+	ld		r18, Z			;(Sparar värdet)
 	add		ZL, r20
 	ld		r17, Z
 	cpi		r17, $00
-	brne	DONE_ROW
-	mov		r16, r20	; Intern loop counter för FULL_ROW_FOUND
-FULL_ROW_FOUND:
+	brne		DONE_ROW
+	mov		r16, r20		; Intern loop counter för FULL_ROW_FOUND
+	FULL_ROW_FOUND:
 	dec		r16			; Kolla raden ovanför den fulla
 	ldi		ZL, LOW(VMEM)
 	add		ZL, r16
-	ld		r17, Z		; Z så ladda och öka ZL
+	ld		r17, Z			; Z så ladda och öka ZL
 	inc		ZL
 	inc		r16			; Återställ countern
 	st		Z, r17
 
 	dec		ZL			; Tillbaks till raden ovanför
-	ldi		r17, $FF	; Nollställ raden
-	st		Z, r17		; Spara nollställningen
-	
+	ldi		r17, $FF		; Nollställ raden
+	st		Z, r17			; Spara nollställningen
+
 	dec		r16
 	cpi		r16, 0
-	brne	FULL_ROW_FOUND
+	brne		FULL_ROW_FOUND
 
-DONE_ROW:
+	DONE_ROW:
 	inc		r20
 	cpi		r20, $0F
-	brne	UPDATE_LOOP
+	brne		UPDATE_LOOP
 
-END_CHECK:
+	END_CHECK:
 	pop		r20
 	pop		r18
 	pop		r17
